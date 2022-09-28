6507ed913177821c5d1bc3d329f8d0facc29e40b
Hadházi Levente <n1ght4ngel19@gmail.com>
2022-09-28 21:28:56 +0200

    Fixed BTS and BHS Switches so that they work for an arbitrary number of Gruntz. (#10)

--- Assets/Scripts/BlueHoldSwitch.cs
+++ Assets/Scripts/BlueHoldSwitch.cs
@@ -1,34 +1,32 @@
+using System.Linq;
+
 using UnityEngine;
 
 public class BlueHoldSwitch : MonoBehaviour {
+  public SpriteRenderer spriteRenderer;
   public bool isPressed;
   public bool isUntouched;
   public Sprite[] animFrames;
 
   private void Start() {
-    isPressed = false;
     isUntouched = true;
   }
 
   private void Update() {
     foreach (Grunt grunt in MapManager.Instance.gruntz) {
       if (
-        grunt.transform.position.x == transform.position.x
-        && grunt.transform.position.y == transform.position.y
+        MapManager.Instance.gruntz.Any(grunt1 => (Vector2)grunt1.transform.position == (Vector2)transform.position)
       ) {
         if (isUntouched) {
+          spriteRenderer.sprite = animFrames[1];
           isUntouched = false;
-
           isPressed = true;
-          gameObject.GetComponent<SpriteRenderer>().sprite = animFrames[1];
         }
-
-        break;
+      } else {
+        spriteRenderer.sprite = animFrames[0];
+        isPressed = false;
+        isUntouched = true;
       }
-
-      isUntouched = true;
-      isPressed = false;
-      gameObject.GetComponent<SpriteRenderer>().sprite = animFrames[0];
     }
   }
 }
--- Assets/Scripts/BlueToggleSwitch.cs
+++ Assets/Scripts/BlueToggleSwitch.cs
@@ -1,32 +1,35 @@
+using System.Linq;
+
 using UnityEngine;
 
 public class BlueToggleSwitch : MonoBehaviour {
+  public SpriteRenderer spriteRenderer;
   public bool isPressed;
   public bool isUntouched;
   public Sprite[] animFrames;
 
   private void Start() {
-    isPressed = false;
     isUntouched = true;
   }
 
   private void Update() {
     foreach (Grunt grunt in MapManager.Instance.gruntz) {
       if (
-        grunt.transform.position.x == transform.position.x
-        && grunt.transform.position.y == transform.position.y
+        MapManager.Instance.gruntz.Any(grunt1 => (Vector2)grunt1.transform.position == (Vector2)transform.position)
       ) {
         if (isUntouched) {
+          spriteRenderer.sprite = animFrames[1];
           isUntouched = false;
-
-          isPressed = !isPressed;
-          gameObject.GetComponent<SpriteRenderer>().sprite = animFrames[1];
+          isPressed = isPressed switch {
+            true => false,
+            false => true
+          };
         }
-
-        break;
+      } else {
+        spriteRenderer.sprite = animFrames[0];
+        isUntouched = true;
       }
-
-      isUntouched = true;
     }
   }
 }
+
