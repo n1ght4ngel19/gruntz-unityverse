d20d471e168f8e3f92c1366a32a6745e6754d42a
n1ght4ngel19 <n1ght4ngel19@gmail.com>
2022-09-29 21:48:59 +0200

    Added Rock prefabs, Script, and break animation.

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Singletonz;
+
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+  public SpriteRenderer spriteRenderer;
+  public List<Sprite> blowUpFrames;
+  public bool isToBeBroken;
+  private bool ranAllFrames;
+
+  private void Update() {
+    if (!isToBeBroken || !isGauntletzGruntAdjacent()) {
+      return;
+    }
+
+    StartCoroutine(blowUpRock());
+      
+    
+  }
+
+  IEnumerator blowUpRock() {
+    foreach (Sprite frame in blowUpFrames) {
+      spriteRenderer.sprite = frame;
+      yield return null;
+    }
+    
+    yield return StartCoroutine(destroyRock());
+  }
+
+  IEnumerator destroyRock() {
+    MapManager.Instance.AddNavTileAt(transform.position);
+    Destroy(gameObject);
+
+    yield return null;
+  }
+
+  private bool isGauntletzGruntAdjacent() {
+    return MapManager.Instance.gruntz
+      .Any(grunt => grunt.tool == ToolType.Gauntletz
+                    && ((Vector2)grunt.transform.position + Vector2.up == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.down == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.left == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.right == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.up + Vector2.left == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.up + Vector2.right == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.down + Vector2.left == (Vector2)transform.position
+                        || (Vector2)grunt.transform.position + Vector2.down + Vector2.right == (Vector2)transform.position)
+      );
+  }
+}
