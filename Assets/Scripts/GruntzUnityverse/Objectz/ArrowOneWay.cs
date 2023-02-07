9180a066817cf6439daba355d10b6cc0924a0d7d
n1ght4ngel19 <n1ght4ngel19@gmail.com>
2023-02-07 20:53:46 +0100

    Added Sprites for TwoWay Arrowz and renamed OneWay Arrow stuff for consistency

--- Assets/Scripts/GruntzUnityverse/Objectz/ArrowOneWay.cs
+++ Assets/Scripts/GruntzUnityverse/Objectz/ArrowOneWay.cs
@@ -6,46 +6,25 @@ using GruntzUnityverse.Managerz;
 using GruntzUnityverse.Utility;
 using UnityEngine;
 
-namespace GruntzUnityverse.Objectz
-{
-  public class Arrow : MonoBehaviour
-  {
+namespace GruntzUnityverse.Objectz {
+  public class ArrowOneWay : MonoBehaviour {
     [field: SerializeField] public Vector2Int OwnLocation { get; set; }
     [field: SerializeField] public CompassDirection Direction { get; set; }
 
 
-    private void Start()
-    {
-      OwnLocation = Vector2Int.FloorToInt
-      (
-        transform.position
-      );
-    }
+    private void Start() { OwnLocation = Vector2Int.FloorToInt(transform.position); }
 
-    private void Update()
-    {
-      foreach (TGrunt grunt in LevelManager.Instance.PlayerGruntz
-        .Where
-        (
-          grunt => grunt.NavComponent.OwnLocation.Equals
-          (
-            OwnLocation
-          )
-        )
-      )
-      {
-        grunt.NavComponent.TargetLocation = OwnLocation
-          + VectorOfDirection
-          (
-            Direction
-          );
+    private void Update() {
+      foreach (TGrunt grunt in LevelManager.Instance.PlayerGruntz.Where(
+        grunt => grunt.NavComponent.OwnLocation.Equals(OwnLocation)
+      )) {
+        grunt.NavComponent.TargetLocation = OwnLocation + VectorOfDirection(Direction);
 
         return;
       }
     }
 
-    public Vector2Int VectorOfDirection(CompassDirection direction)
-    {
+    public Vector2Int VectorOfDirection(CompassDirection direction) {
       return direction switch {
         CompassDirection.North => Vector2IntC.North,
         CompassDirection.East => Vector2IntC.East,
@@ -55,12 +34,7 @@ namespace GruntzUnityverse.Objectz
         CompassDirection.NorthWest => Vector2IntC.NorthWest,
         CompassDirection.SouthEast => Vector2IntC.SouthEast,
         CompassDirection.SouthWest => Vector2IntC.SouthWest,
-        var _ => throw new ArgumentOutOfRangeException
-        (
-          nameof(direction),
-          direction,
-          "No Arrow direction specified!"
-        ),
+        var _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, "No Arrow direction specified!"),
       };
     }
   }
