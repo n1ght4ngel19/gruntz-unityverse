f80ce35daacf93365440154eb69ac820a42fcc57
n1ght4ngel19 <n1ght4ngel19@gmail.com>
2023-02-07 18:43:31 +0100

    Added OneTime Switch and Black Pyramid

---
+++ Assets/Scripts/GruntzUnityverse/Objectz/Switchez/OneTimeSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GruntzUnityverse.Managerz;
+using GruntzUnityverse.Objectz.Pyramidz;
+using UnityEngine;
+
+namespace GruntzUnityverse.Objectz.Switchez {
+  public class OneTimeSwitch : MonoBehaviour {
+    [field: SerializeField] public Vector2Int OwnLocation { get; set; }
+    private SpriteRenderer Renderer { get; set; }
+    [field: SerializeField] public Sprite PressedSprite { get; set; }
+    [field: SerializeField] public Sprite ReleasedSprite { get; set; }
+    [field: SerializeField] public List<BlackPyramid> Pyramidz { get; set; }
+    [field: SerializeField] public bool IsPressed { get; set; }
+
+
+    private void Start() {
+      if (Pyramidz.Count.Equals(0)) {
+        Debug.LogWarning("There is no Pyramid assigned to this Switch!");
+      }
+
+      OwnLocation = Vector2Int.FloorToInt(transform.position);
+      Renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+      if (LevelManager.Instance.PlayerGruntz.Any(grunt => grunt.NavComponent.OwnLocation.Equals(OwnLocation))) {
+        TogglePyramidz();
+
+        IsPressed = true;
+        Renderer.sprite = PressedSprite;
+        enabled = false;
+      }
+    }
+
+    private void TogglePyramidz() {
+      foreach (BlackPyramid pyramid in Pyramidz) {
+        pyramid.ChangeState();
+        pyramid.enabled = false;
+      }
+    }
+  }
+}
