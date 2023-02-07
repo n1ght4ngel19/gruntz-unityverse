060a3198157c5ccf3e5957384c1f815c83eaafb3
n1ght4ngel19 <n1ght4ngel19@gmail.com>
2023-02-07 22:41:05 +0100

    Added TwoWay Arrowz with Yellow Toggle and Hold Switchez

---
+++ Assets/Scripts/GruntzUnityverse/Objectz/Switchez/YellowArrowToggleSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GruntzUnityverse.Managerz;
+using GruntzUnityverse.Objectz.Arrowz;
+using UnityEngine;
+
+namespace GruntzUnityverse.Objectz.Switchez {
+  public class YellowArrowToggleSwitch : ObjectSwitch {
+    [field: SerializeField] public bool HasBeenPressed { get; set; }
+    [field: SerializeField] public List<TwoWayArrow> Arrowz { get; set; }
+
+
+    private void Update() {
+      if (LevelManager.Instance.PlayerGruntz.Any(grunt => grunt.NavComponent.OwnLocation.Equals(OwnLocation))) {
+        if (!HasBeenPressed) {
+          ToggleArrowz();
+
+          IsPressed = true;
+          HasBeenPressed = true;
+          Renderer.sprite = PressedSprite;
+        }
+      } else {
+        IsPressed = false;
+        HasBeenPressed = false;
+        Renderer.sprite = ReleasedSprite;
+      }
+    }
+
+    private void ToggleArrowz() {
+      foreach (TwoWayArrow arrow in Arrowz) {
+        arrow.ChangeDirection();
+      }
+    }
+  }
+}
