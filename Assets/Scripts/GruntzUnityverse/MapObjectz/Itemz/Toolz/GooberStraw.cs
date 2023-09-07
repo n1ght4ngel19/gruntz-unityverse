3f68125ead409316b40ac79dc6854ef498e90b7f
n1ght4ngel19 <levi.hadhazi@gmail.com>
2023-09-07 19:01:46 +0200

    Corrections and adjustments #3; GooberStraw; GooberStraw Grunt; Checkpoint Flag

---
+++ Assets/Scripts/GruntzUnityverse/MapObjectz/Itemz/Toolz/GooberStraw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using GruntzUnityverse.Enumz;
+
+namespace GruntzUnityverse.MapObjectz.Itemz.Toolz {
+  public class GooberStraw : Tool {
+    protected override void Start() {
+      base.Start();
+
+      toolName = ToolName.GooberStraw;
+      toolRange = RangeType.Melee;
+      damage = GlobalValuez.GooberStrawDamage;
+      mapItemName = nameof(GooberStraw);
+    }
+    // -------------------------------------------------------------------------------- //
+
+    public override IEnumerator UseTool() {
+      // Todo: Implement
+      yield return null;
+    }
+  }
+}
---
+++ Assets/Scripts/GruntzUnityverse/StartScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace GruntzUnityverse {
+  public class StartScene : MonoBehaviour {
+    private void Awake() {
+      Addressables.LoadSceneAsync("Menuz/MainMenu.unity");
+    }
+  }
+}
