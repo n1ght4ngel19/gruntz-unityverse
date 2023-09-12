d28dd5e196376d0400c7703a4effcd73f06a0562
n1ght4ngel19 <levi.hadhazi@gmail.com>
2023-09-12 13:23:30 +0200

    Added prefabs for static mapobjects; Added mostly finished RR Level 3; Updated template level

---
+++ Assets/Scripts/GruntzUnityverse/ImmovableGameObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GruntzUnityverse {
+  public class ImmovableGameObject : MonoBehaviour {
+    private void OnValidate() {
+      transform.hideFlags = HideFlags.NotEditable;
+    }
+  }
+}
