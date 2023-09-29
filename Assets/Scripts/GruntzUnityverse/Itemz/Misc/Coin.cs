d70bcb9cffed90707e5f2aefcf72200fb50978ff
Hadházi Levente <levi.hadhazi@gmail.com>
2023-09-29 22:46:30 +0200

    Remade pickup anims with correct offsets

---
+++ Assets/Scripts/GruntzUnityverse/Itemz/Misc/Coin.cs
@@ -0,0 +1,12 @@
+﻿using GruntzUnityverse.MapObjectz.BaseClasses;
+
+namespace GruntzUnityverse.MapItemz.Misc {
+  public class Coin : Item {
+    protected override void Start() {
+      base.Start();
+
+      category = "Misc";
+      mapItemName = nameof(Coin);
+    }
+  }
+}
---
+++ Assets/Scripts/GruntzUnityverse/Itemz/Misc/Warpletter.cs
@@ -0,0 +1,15 @@
+﻿using GruntzUnityverse.Enumz;
+using GruntzUnityverse.MapObjectz.BaseClasses;
+
+namespace GruntzUnityverse.MapItemz.Misc {
+  public class Warpletter : Item {
+    public WarpletterType warpletterType;
+
+    protected override void Start() {
+      base.Start();
+
+      category = "Misc";
+      mapItemName = nameof(Warpletter);
+    }
+  }
+}
